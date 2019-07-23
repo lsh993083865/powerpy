1d8f899d82b07fa706d4675601741efa643e8f4d
lushihao <993083865@qq.com>
2019-07-23 20:18:30 +0800

    test

---
+++ demo01.py
@@ -0,0 +1,22 @@
+lint = 616 #整数
+lstr = "0616" #字符串
+lflout = 0.616 #小数
+llist = [] #列表
+lbool = False #布尔值
+ltuple = () #元组
+ldict = {} #字典
+lnone = None #空
+
+bltype = type(lnone)
+print(lnone)
+print(bltype)
+
+print("425125"+"5346534")
+
+ltuple = (155,"456",48.485,155,"456",48.485155,"456",48.485)
+
+print(ltuple[1])
+num = ltuple.count(155)
+print(num)
+xxx = ltuple.index("456")
+print(xxx)
